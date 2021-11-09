dcbd0e51c33013745259da087602c6ab727dd1bc
Tom Moyse <Tom.Moyse@warwick.ac.uk>
2021-11-09 13:29:33 +0000

    Dynamic array is at least partly broken

--- src/analysis.c
+++ src/analysis.c
@@ -7,15 +7,77 @@
 #include <signal.h>
 #include <stdlib.h>
 
+typedef struct dyn_arr_ip{
+	in_addr_t *arr;
+	size_t used;
+	size_t max;
+} dyn_arr_ip;
+
 static int syn_packets = 0;
-static in_addr_t ip_addresses[50];
+static dyn_arr_ip ip_addresses;
+static int is_initialised = 0;
+
+void init_arr_ip(dyn_arr_ip *arr, size_t size){
+	arr->arr = malloc(size * sizeof(in_addr_t));
+	arr->used = 0;
+	arr->max = size;
+}
+
+void append_arr_ip(dyn_arr_ip *arr, in_addr_t ip){
+	if (arr->used == arr->max){
+		arr->max = arr->max * 2;
+		arr->arr = realloc(arr->arr, arr->max * sizeof(in_addr_t));
+	}
+	arr->arr[arr->used] = ip;
+	arr->used = arr->used + 1;
+}
+
+void free_arr_ip(dyn_arr_ip *arr){
+	free(arr->arr);
+	arr->arr = NULL;
+	arr->used = 0;
+	arr->max = 0;
+}
+
+in_addr_t get_arr_ip(dyn_arr_ip *arr, int index){
+	if (arr->used >= index){ return NULL; }
+	return arr->arr[index];
+}
 
 void exit_callback(int signum){
-    printf("%d SYN packets detected", syn_packets);
+    printf("%d SYN packets detected\n", syn_packets);
+	// To find number of unique IP's first make sorted array
+	in_addr_t uniques[(&ip_addresses)->used];
+	int length = 0;
+	int found;
+	in_addr_t address;
+
+	for (int i = 0; i < (&ip_addresses)->used; i++){
+		found = 0;
+		address = get_arr_ip(&ip_addresses, i);
+		printf("%d",address);
+		for (int j = 0; j < length; j++){
+			if (uniques[j] == address){
+				found = 1;
+				break;
+			}
+		}
+		if (!found){
+			uniques[length] = address;
+			length++;
+		}
+	}
+
+	printf("%d Unique IP source addresses\n, %d", length, uniques[0]);
+
 	exit(signum);
 }
 
 void analyse(struct pcap_pkthdr *header, const unsigned char *packet, int verbose) {
+	if (!is_initialised){
+		init_arr_ip(&ip_addresses, 5);
+		is_initialised = 1;
+	}
 	// Process Ethernet header
 	struct ether_header *eth_header = (struct ether_header *) packet;
 
@@ -25,6 +87,9 @@ void analyse(struct pcap_pkthdr *header, const unsigned char *packet, int verbos
 	u_int8_t ip_protocol = ip_header->ip_p;
 	struct in_addr ip_src = ip_header->ip_src;
 	struct in_addr ip_dst = ip_header->ip_dst;
+
+	char mystring[50];
+	printf("IP source: %s\n", inet_ntop(AF_INET, &(ip_src.s_addr), &mystring, 50));
 
 	if (ip_protocol == IPPROTO_TCP){
 		//Process TCP header
@@ -44,12 +109,12 @@ void analyse(struct pcap_pkthdr *header, const unsigned char *packet, int verbos
 		u_int16_t tcp_src = tcp_header->th_sport;
 		u_int16_t tcp_dst = tcp_header->th_dport;
 		unsigned int tcp_length = (tcp_header->th_off) * 4;
-		printf("\n%d",(tcp_header->th_flags & TH_SYN));
 
-		if ((tcp_header->th_flags & TH_SYN)){
+		// Use equals opposed to & as want to check exclusively syn bit set
+		if (tcp_header->th_flags == TH_SYN){
 			// Is SYN packet
 			if (syn_packets < 50){
-				ip_addresses[syn_packets] = ip_src.s_addr;
+				append_arr_ip(&ip_addresses, ip_src.s_addr);
 			}
 			syn_packets++;
 		}
