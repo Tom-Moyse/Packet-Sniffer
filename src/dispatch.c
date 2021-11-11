bf004fe6039df0cbd295935bab890ff8148bdbc8
Tom Moyse <Tom.Moyse@warwick.ac.uk>
2021-11-11 17:47:35 +0000

    Began work on threadpool implementation

--- src/dispatch.c
+++ src/dispatch.c
@@ -1,20 +1,14 @@
 #include "dispatch.h"
 
 #include <pcap.h>
+#include <pthread.h>
 
 #include "analysis.h"
 
-void dispatch(struct pcap_pkthdr *header,
-              const unsigned char *packet,
-              int verbose) {
-    // TODO: Your part 2 code here
-    // This method should handle dispatching of work to threads. At present
-    // it is a simple passthrough as this skeleton is single-threaded.
-    analyse(header, packet, verbose);
-}
-
 struct node {  // data structure for each node
-    int item;
+    struct pcap_pkthdr *header;
+    const unsigned char *packet;
+    int verbose;
     struct node *next;
 };
 
@@ -41,9 +35,11 @@ int isempty(struct queue *q) {  // checks if queue is empty
     return (q->head == NULL);
 }
 
-void enqueue(struct queue *q, int item) {  //enqueues a node with an item
+void enqueue(struct queue *q, struct pcap_pkthdr *header, const unsigned char *packet, int verbose) {  //enqueues a node with an item
     struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    new_node->item = item;
+    new_node->header = header;
+    new_node->packet = packet;
+    new_node->verbose = verbose;
     new_node->next = NULL;
     if (isempty(q)) {
         q->head = new_node;
@@ -67,37 +63,7 @@ void dequeue(struct queue *q) {  //dequeues a the head node
     }
 }
 
-void printqueue(struct queue *q) {
-    if (isempty(q)) {
-        printf("The queue is empty\n");
-    } else {
-        struct node *read_head;
-        read_head = q->head;
-        printf("The queue elements from head to tail are:\n");
-        printf("%d", read_head->item);
-        while (read_head->next != NULL) {
-            read_head = read_head->next;
-            printf("--> %d", read_head->item);
-        }
-        printf("\n");
-    }
-}
-
-#include <arpa/inet.h>
-#include <ctype.h>
-#include <netdb.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-
-#define BUFSIZE 2048
-#define BACKLOG 500
 #define NUMTHREADS 10
-#define PORTNO 8888
-
 /* Queue where the main server thread adds work and from where the 
 worker threads pull work*/
 struct queue *work_queue;
@@ -109,12 +75,9 @@ pthread_cond_t queue_cond = PTHREAD_COND_INITIALIZER;
 
 /*Function to be executed by each worker thread*/
 void *handle_conn(void *arg) {
-    int recvlen;
-    int conn_sock;
-    char buf[BUFSIZE];
-    long long limit;
-    long long sum = 0, i;
-
+    struct pcap_pkthdr *header;
+    unsigned char *packet;
+    int verbose;
     /* In a loop continue checking if any more work is left to be done*/
     while (1) {
         // acquire lock, get connection socket descriptor from work queue, release lock
@@ -123,29 +86,40 @@ void *handle_conn(void *arg) {
         while (isempty(work_queue)) {
             pthread_cond_wait(&queue_cond, &queue_mutex);
         }
-        conn_sock = work_queue->head->item;
+        header = work_queue->head->header;
+        packet = work_queue->head->packet;
+        verbose = work_queue->head->verbose;
         dequeue(work_queue);
         pthread_mutex_unlock(&queue_mutex);
 
-        // read from connetion socket into buffer
-        recvlen = read(conn_sock, &limit, sizeof(limit));
-
-        // print received message
-        if (recvlen > 0) {
-            printf("Received number by thread %d: %lld\n", (int)pthread_self(), limit);
-        } else {
-            printf("uh oh - something went wrong!\n");
-        }
-
-        sum = 0;
-        for (i = 1; i <= limit; i++) {  //compute the sum
-            sum += i;
-        }
-        // send back to the sender by writing to the conneection socket
-        write(conn_sock, &sum, sizeof(sum));
+        analyse(header, packet, verbose);
     }
     return NULL;
 }
+
+
+void dispatch(struct pcap_pkthdr *header,
+              const unsigned char *packet,
+              int verbose) {
+    // TODO: Your part 2 code here
+    // This method should handle dispatching of work to threads. At present
+    // it is a simple passthrough as this skeleton is single-threaded.
+    analyse(header, packet, verbose);
+}
+
+#include <ctype.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFSIZE 2048
+#define BACKLOG 500
+#define PORTNO 8888
+
+
 
 int main(int argc, char **argv) {
     struct sockaddr_in myaddr;           /* our address */
