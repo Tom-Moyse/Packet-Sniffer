abf29881db12f50c129bc3457464ccbcc4ef6ea5
Tom Moyse <Tom.Moyse@warwick.ac.uk>
2021-11-15 15:19:48 +0000

    Nearly complete, completely untested

--- src/dispatch.c
+++ src/dispatch.c
@@ -7,15 +7,21 @@
 #include "analysis.h"
 
 typedef struct stored_packet_queue{
-    packet_node* head;
-    packet_node* tail;
+    packet_q_node* head;
+    packet_q_node* tail;
 } stored_packet_queue;
+
+typedef struct packet_q_node{
+    struct pcap_pkthdr header;
+    unsigned char* data_start;
+    unsigned int data_length; 
+    struct packet_node* next;
+} packet_q_node;
 
 typedef struct packet_node{
     struct pcap_pkthdr header;
     unsigned char* data_start;
     unsigned int data_length; 
-    struct packet_node* next;
 } packet_node;
 
 int is_empty(stored_packet_queue *q){
@@ -29,7 +35,7 @@ int tail_empty(stored_packet_queue *q){
 void store_packet(stored_packet_queue *q, const struct pcap_pkthdr *header, const unsigned char *packet){
     // Allocate resources and create packet
     unsigned int packet_size = header->len;
-    packet_node* new_packet = malloc(sizeof(packet_node));
+    packet_q_node* new_packet = malloc(sizeof(packet_q_node));
     unsigned char* packet_data = malloc(sizeof(unsigned char) * packet_size);
     memcpy(packet_data, packet, packet_size);
     new_packet->header = *header;
@@ -61,7 +67,7 @@ int remove_packet(stored_packet_queue *q){
         q->head = NULL;
         return EXIT_SUCCESS;
     }
-    packet_node *current_head = q->head;
+    packet_q_node *current_head = q->head;
     q->head = q->head->next;
     if (q->head == q->tail){
         q->tail = NULL;
@@ -72,15 +78,25 @@ int remove_packet(stored_packet_queue *q){
     return EXIT_SUCCESS;
 }
 
-#define NUMTHREADS 1
+int get_analysis_index(int* new_packets, int size){
+    for (int i = 0; i < size; i++){
+        if (new_packets[i]){ return i; }
+    }
+    return -1;
+}
+
+#define NUMTHREADS 2
 
 stored_packet_queue packet_queue;
 packet_node *to_analyse;
+int new_packets[NUMTHREADS];
+int analysis_index = 0;
 
 void init_structures(){
-    to_analyse = calloc(NUMTHREADS, 1500);
     packet_queue.head = NULL;
     packet_queue.tail = NULL;
+    to_analyse = malloc(NUMTHREADS * sizeof(packet_node));
+    memset(new_packets, 1, sizeof(new_packets));
 }
 
 /* mutex lock required for the shared queue*/
@@ -94,15 +110,40 @@ static int end_analysis = 0;
 
 /*Function to be executed by each analysis thread*/
 void *handle_analyse(void *arg) {
+    printf("Thread id: %d", pthread_self());
     return NULL;
 }
 
 /*Function to be executed by allocation thread*/
 void *handle_allocate(void *arg) {
-    while (1){
+    int p_index;
+    while (1) {
         pthread_mutex_lock(&queue_mutex);
+        while (is_empty(&packet_queue)) {
+            pthread_cond_wait(&queue_cond, &queue_mutex);
+        }
+        if (p_index = get_analysis_index(new_packets, NUMTHREADS) == -1){
+            continue;
+        }
+        // Transfer packet from queue to packet struct in to_analyse at given index
+        to_analyse[p_index].header = packet_queue.head->header;
+        to_analyse[p_index].data_length = packet_queue.head->data_length;
+        to_analyse[p_index].data_start = malloc(packet_queue.head->data_length);
+        memcpy(to_analyse[p_index].data_start, packet_queue.head->data_start, packet_queue.head->data_length);
+
+        // Indicate to thread that there's a new packet to analyse
+        new_packets[p_index] = 1;
+
+        // Remove packet from queue
+        remove_packet(&packet_queue);
+
+        pthread_mutex_unlock(&queue_mutex);
+
+        if (end_analysis){
+            //free(packet);
+            return NULL;
+        }
     }
-    
     return NULL;
 }
 
